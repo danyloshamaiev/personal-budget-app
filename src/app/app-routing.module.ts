95d218db99e151669017494a6af3b9d7e09ee76f
Danylo Shamaiev <danyloshamaiev@gmail.com>
2022-09-28 18:02:20 +0300

    Generate modules and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) }, { path: 'accounts', loadChildren: () => import('./accounts/accounts.module').then(m => m.AccountsModule) }, { path: 'cash', loadChildren: () => import('./cash/cash.module').then(m => m.CashModule) }, { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
