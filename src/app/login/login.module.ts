bacf8a9a6eb74ef2b238e511cb24960068b3d60c
Danylo Shamaiev <danyloshamaiev@gmail.com>
2022-10-04 11:26:01 +0300

    Add Login page markup

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ButtonModule} from 'primeng/button';
+import {CheckboxModule} from 'primeng/checkbox';
+import {InputTextModule} from 'primeng/inputtext';
+import {RippleModule} from 'primeng/ripple';
 
 import {LoginRoutingModule} from './login-routing.module';
 import {LoginComponent} from './login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule],
+  imports: [CommonModule, LoginRoutingModule, InputTextModule, CheckboxModule, ButtonModule, RippleModule],
 })
 export class LoginModule {}
