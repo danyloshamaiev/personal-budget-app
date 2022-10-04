fd2d02a7a68c302a038c8a0a59e7ed38039ded95
Danylo Shamaiev <danyloshamaiev@gmail.com>
2022-10-04 11:50:44 +0300

    Register page markup

--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ButtonModule} from 'primeng/button';
+import {CheckboxModule} from 'primeng/checkbox';
+import {InputTextModule} from 'primeng/inputtext';
+import {RippleModule} from 'primeng/ripple';
 
 import {RegisterRoutingModule} from './register-routing.module';
 import {RegisterComponent} from './register.component';
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, RegisterRoutingModule],
+  imports: [CommonModule, RegisterRoutingModule, ButtonModule, RippleModule, CheckboxModule, InputTextModule],
 })
 export class RegisterModule {}
