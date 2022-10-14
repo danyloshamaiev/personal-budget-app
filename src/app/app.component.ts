8fa42e6977f6fcf7032cd81c73beb72e9fbb4d35
Danylo Shamaiev <danyloshamaiev@gmail.com>
2022-10-14 11:14:02 +0300

    Hide Logout button if not authenticated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {PrimeNGConfig} from 'primeng/api';
+import {Observable} from 'rxjs';
 import {AuthService} from './services/auth.service';
 import {ThemeService} from './services/theme.service';
 
@@ -8,11 +9,15 @@ import {ThemeService} from './services/theme.service';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
+  public isAuthenticated$: Observable<boolean>;
+
   constructor(
     private primengConfig: PrimeNGConfig,
     private themeService: ThemeService,
     private authService: AuthService
-  ) {}
+  ) {
+    this.isAuthenticated$ = this.authService.isAuthenticated$;
+  }
 
   ngOnInit() {
     this.primengConfig.ripple = true;
