8d0aa0e2c40e20ce1be5004fed92719351609e63
Danylo Shamaiev <danyloshamaiev@gmail.com>
2022-10-14 11:13:38 +0300

    Remove unused code

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -24,7 +24,7 @@ import {EMPTY, Observable} from 'rxjs';
 import IUser from '../models/user.model';
 import {delay, map, filter, switchMap} from 'rxjs/operators';
 import {Router} from '@angular/router';
-import {ActivatedRoute, NavigationEnd} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 import {ToastService} from './toast.service';
 
 @Injectable({
@@ -35,7 +35,6 @@ export class AuthService {
   public isAuthenticated$: Observable<boolean>;
   public isAuthenticatedWithDelay$: Observable<boolean>;
   private usersCollection: CollectionReference;
-  private redirect = false;
 
   constructor(
     private auth: Auth,
@@ -48,15 +47,6 @@ export class AuthService {
     this.usersCollection = collection(this.db, 'users');
     this.isAuthenticated$ = this.user$.pipe(map((user) => !!user));
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
-    this.router.events
-      .pipe(
-        filter((e) => e instanceof NavigationEnd),
-        map((e) => this.route.firstChild),
-        switchMap((route) => route?.data ?? EMPTY)
-      )
-      .subscribe((data) => {
-        this.redirect = data['authOnly'] ?? false;
-      });
   }
 
   public signInWithGoogle() {
@@ -103,9 +93,7 @@ export class AuthService {
 
     await signOut(this.auth);
 
-    if (this.redirect) {
-      await this.router.navigateByUrl('/');
-    }
+    await this.router.navigateByUrl('/login');
   }
 
   private handleAuthSuccess(result: UserCredential): void {
