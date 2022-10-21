1b6011f15d0136bbecf143839c19e96738a13512
Danylo Shamaiev <danyloshamaiev@gmail.com>
2022-10-21 11:13:01 +0300

    Make application a PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import {environment} from '../environments/environment';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -33,6 +34,12 @@ import {AppComponent} from './app.component';
     ButtonModule,
     RippleModule,
     AvatarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [MessageService],
   bootstrap: [AppComponent],
